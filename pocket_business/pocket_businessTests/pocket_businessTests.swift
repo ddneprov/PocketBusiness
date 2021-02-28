35a3e387a86f0e07fab7a84f4ccfe2ff2e25faea
Dneprov <44896272+ddneprov@users.noreply.github.com>
2021-02-28 19:52:18 +0300

    Add files via upload

---
+++ pocket_business/pocket_businessTests/pocket_businessTests.swift
@@ -0,0 +1,34 @@
+//
+//  pocket_businessTests.swift
+//  pocket_businessTests
+//
+//  Created by 18177231 on 29/01/2021.
+//  Copyright © 2021 ddneprov. All rights reserved.
+//
+
+import XCTest
+@testable import pocket_business
+
+class pocket_businessTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
