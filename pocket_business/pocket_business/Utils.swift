d91eb150383e46a2855e2e6e61b5b508a21bc587
ddneprov <ddneprov_24@mail.ru>
2021-04-22 16:49:00 +0300

    pomogite

---
+++ pocket_business/pocket_business/Utils.swift
@@ -0,0 +1,36 @@
+//
+//  Utils.swift
+//  pocket_business
+//
+//  Created by 18177231 on 21.04.2021.
+//  Copyright © 2021 ddneprov. All rights reserved.
+//
+
+import Foundation
+
+public class Utils{
+    
+    func getDate() -> String{
+        let date = Date()
+        let format = DateFormatter()
+        format.dateFormat = "dd, EEEE"
+        return format.string(from: date)
+    }
+    
+    
+    func parseJSONtoProduct(data: String) -> [Product]? {
+        
+        var returnValue: [Product]?
+        let jsonData = Data(data.utf8)
+        
+        do {
+            returnValue = try JSONDecoder().decode([Product].self, from: jsonData)
+        } catch {
+            print("Error took place\(error.localizedDescription).")
+        }
+        
+        return returnValue
+    }
+    
+}
+
