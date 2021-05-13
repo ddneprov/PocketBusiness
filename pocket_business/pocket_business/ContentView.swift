3fc93ebb74613f4780b18701724d7c5bff772556
ddneprov <ddneprov_24@mail.ru>
2021-05-13 23:40:12 +0300

    mobile log in

--- pocket_business/pocket_business/ContentView.swift
+++ pocket_business/pocket_business/ContentView.swift
@@ -11,53 +11,188 @@ import SwiftUICharts
 
 
 
-
-struct ContentView: View {
+struct BottomTab: View {
     var body: some View {
+                        TabView {
+                            TodayView()
+                                     .tabItem {
+                                        Image(systemName: "calendar")
+                                        Text("Сегодня")
+                                      }
         
-        TabView {
-            TodayView()
-                     .tabItem {
-                        Image(systemName: "calendar")
-                        Text("Сегодня")
-                      }
-
-            TopView()
-                     .tabItem {
-                        Image(systemName: "arrowtriangle.up.square.fill")
-                        Text("Топ")
-                      }
-                
-            ThirdView()
-                     .tabItem {
-                        Image(systemName: "chart.bar.xaxis")
-                        Text("Метрики")
-                      }
-                
-            PeopleView()
-                     .tabItem {
-                        Image(systemName: "person.3.fill")
-                        Text("Люди")
-                      }
-            
-            BankView()
-                     .tabItem {
-                        Image(systemName: "rublesign.circle.fill")
-                        Text("Деньги")
-                      }
+                            TopView()
+                                     .tabItem {
+                                        Image(systemName: "arrowtriangle.up.square.fill")
+                                        Text("Метрики")
+                                      }
+        
+                            PeopleView()
+                                     .tabItem {
+                                        Image(systemName: "person.3.fill")
+                                        Text("Люди")
+                                      }
+        
+                            BankView()
+                                     .tabItem {
+                                        Image(systemName: "rublesign.circle.fill")
+                                        Text("Деньги")
+                                      }
+    }
+}
+}
+    
+struct ContentView: View {
+    
+    @State var signInSuccess = false
+    
+    
+    var body: some View {
+        return Group {
+            if signInSuccess {
+                BottomTab()
             }
+            else {
+                LogIn(signInSuccess: $signInSuccess)
+                //LoginFormView(signInSuccess: $signInSuccess)
+            }
+        }
+    }
 }
 
 
+/*
+struct ContentView: View {
+    @State var signInSuccess = false
+    
+    var body: some View {
+            return Group {
+                    if signInSuccess {
+                        TabView {
+                            TodayView()
+                                     .tabItem {
+                                        Image(systemName: "calendar")
+                                        Text("Сегодня")
+                                      }
 
+                            TopView()
+                                     .tabItem {
+                                        Image(systemName: "arrowtriangle.up.square.fill")
+                                        Text("Топ")
+                                      }
+                                
+                            ThirdView()
+                                     .tabItem {
+                                        Image(systemName: "chart.bar.xaxis")
+                                        Text("Метрики")
+                                      }
+                                
+                            PeopleView()
+                                     .tabItem {
+                                        Image(systemName: "person.3.fill")
+                                        Text("Люди")
+                                      }
+                            
+                            BankView()
+                                     .tabItem {
+                                        Image(systemName: "rublesign.circle.fill")
+                                        Text("Деньги")
+                                      }
+                            }
+                    }
+                    else {
+                        LogIn()
+//                        LoginFormView(signInSuccess: $signInSuccess)
+                    }
+                }
+        
+
+    }
+}
+*/
+
+    struct LogIn: View {
+        
+        
+        @State var user = ""
+        @State var pass = ""
+                
+        @Binding var signInSuccess: Bool
+        @State private var showError = false
+        
+        var body : some View{
+                VStack {
+                     Text("Sign In").fontWeight(.heavy).font(.largeTitle).padding([.top,.bottom], 20)
+                    VStack{
+                        
+                        VStack(alignment: .leading){
+                            
+                            VStack(alignment: .leading){
+                                
+                                Text("Username").font(.headline).fontWeight(.light).foregroundColor(Color.init(.label).opacity(0.75))
+                                
+                                HStack{
+                                    
+                                    TextField("Enter Your Username", text: $user)
+                                    
+                                    if user != ""{
+                                        
+                                        Image("check").foregroundColor(Color.init(.label))
+                                    }
+                                    
+                                }
+                                
+                                Divider()
+                                
+                            }.padding(.bottom, 15)
+                            
+                            VStack(alignment: .leading){
+                                
+                                Text("Password").font(.headline).fontWeight(.light).foregroundColor(Color.init(.label).opacity(0.75))
+                                
+                                SecureField("Enter Your Password", text: $pass)
+                                
+                                Divider()
+                            }
+                              
+                        }.padding(.horizontal, 6)
+                     
+                    }.padding()
+                    VStack{
+                        
+                        Button(action: {
+                                        // Your auth logic
+                                        if(self.user == self.pass) {
+                                            self.signInSuccess = true
+                                        }else {
+                                            self.showError = true
+                                        }
+                                    }) {
+                                        Text("Sign In").foregroundColor(.black).frame(width: UIScreen.main.bounds.width - 120).padding()
+                                    }
+                                    
+                                    if showError {
+                                       Text("Incorrect username/password").foregroundColor(Color.red)
+                                   }
+                            
+                            
+                        }.background(Color("color"))
+                            .clipShape(Capsule())
+                            .padding(.top, 45)
+                        
+                        
+                }
+            }
+        }
+    
 
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
+            //LogIn()
             ContentView()
         }
     }
 }
-}
+
