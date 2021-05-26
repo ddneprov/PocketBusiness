4eb11af9bdfb5006763be6c571500be91cbe8539
Dneprov <44896272+ddneprov@users.noreply.github.com>
2021-05-26 06:51:01 +0300

    1с

---
+++ serverPocketBusiness/src/main/java/com/example/serverPocketBusiness/controller/IntegrationController.java
@@ -0,0 +1,10 @@
+package com.example.serverPocketBusiness.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/1c")
+public class IntegrationController {
+}
---
+++ serverPocketBusiness/src/main/java/com/example/serverPocketBusiness/orchestration/IntegrationOrchestration.java
@@ -0,0 +1,8 @@
+package com.example.serverPocketBusiness.orchestration;
+
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class IntegrationOrchestration {
+}
