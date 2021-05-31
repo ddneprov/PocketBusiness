7a388255b997e5944fda2bc46029ccbb82aeef2c
Dneprov <44896272+ddneprov@users.noreply.github.com>
2021-05-31 16:16:11 +0300

    date

--- serverPocketBusiness/src/main/java/com/example/serverPocketBusiness/orchestration/SaleOrchestration.java
+++ serverPocketBusiness/src/main/java/com/example/serverPocketBusiness/orchestration/SaleOrchestration.java
@@ -9,8 +9,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
